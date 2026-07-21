57e788e70f167c788ad354bcc71d5587ed5a92f4
axmedjaa <axmedja44@gmail.com>
2026-07-21 19:34:55 +0300

    updated theme

--- frontend/src/components/mode-toggle.jsx
+++ frontend/src/components/mode-toggle.jsx
@@ -1,23 +1,15 @@
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
-
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
   const isDark = theme === "dark";
   return (
     <Button
       variant="outline"
-      size="icon"
       onClick={() => setTheme(isDark ? "light" : "dark")}
-      aria-label="Toggle theme"
-      className="rounded-full"
     >
-      {isDark ? (
-        <SunIcon className="h-5 w-5" />
-      ) : (
-        <MoonIcon className="h-5 w-5" />
-      )}
+      {isDark ? <SunIcon /> : <MoonIcon />}
     </Button>
   );
 }
